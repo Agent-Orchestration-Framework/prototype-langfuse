bab8b4bfc2728a6ef58c7252f9a3b0ff1b9cf5e0
thach <jonathan.thach@etika.com>
2025-05-28 15:23:20 +1000

    langfuse only with no otel collector, combined trace

--- examples/otelCollector.ts
+++ examples/otelCollector.ts
@@ -13,55 +13,55 @@ import { layoutChain } from "./util";
 const [, , input] = process.argv;
 
 async function runSimple() {
-  const langfuseHandler = new CallbackHandler();
+      const langfuseHandler = new CallbackHandler();
 
-  const model = new ChatOpenAI({
-    model: "gpt-4o-mini",
-    temperature: 0,
-  });
+      const model = new ChatOpenAI({
+        model: "gpt-4o-mini",
+        temperature: 0,
+      });
 
-  const requestRouterPrompt = PromptTemplate.fromTemplate(
-    `Classify the user interface request into one of the options
+      const requestRouterPrompt = PromptTemplate.fromTemplate(
+        `Classify the user interface request into one of the options
 
 Only use the options mentioned in the 'Classification' function.
 
 User interface request:
 {input}`
-  ).withConfig({
-    runName: "Request Router Prompt",
-  });
+      ).withConfig({
+        runName: "Request Router Prompt",
+      });
 
-  const uiClassification = z.object({
-    requestType: z.enum(["COMPONENT", "LAYOUT"]),
-  });
+      const uiClassification = z.object({
+        requestType: z.enum(["COMPONENT", "LAYOUT"]),
+      });
 
-  const requestRouter = requestRouterPrompt
-    .pipe(
-      model.withStructuredOutput(uiClassification).withConfig({
-        runName: "Send to LLM",
-      })
-    )
-    .withConfig({
-      runName: "Request Router",
-    });
+      const requestRouter = requestRouterPrompt
+        .pipe(
+          model.withStructuredOutput(uiClassification).withConfig({
+            runName: "Send to LLM",
+          })
+        )
+        .withConfig({
+          runName: "Request Router",
+        });
 
-  const runnable = RunnableSequence.from([
-    RunnablePassthrough.assign({ request: requestRouter }).withConfig({
-      runName: "Request Router Passthrough",
-    }),
-    (args) => {
-      if (args.request.requestType === "COMPONENT") {
-        console.log("A component was requested.");
-      } else {
-        console.log("A layout was requested");
-        return layoutChain;
-      }
+      const runnable = RunnableSequence.from([
+        RunnablePassthrough.assign({ request: requestRouter }).withConfig({
+          runName: "Request Router Passthrough",
+        }),
+        (args) => {
+          if (args.request.requestType === "COMPONENT") {
+            console.log("A component was requested.");
+          } else {
+            console.log("A layout was requested");
+            return layoutChain;
+          }
 
-      return args;
-    },
-  ]).withConfig({
-    runName: "UI request chain",
-  });
+          return args;
+        },
+      ]).withConfig({
+        runName: "UI request chain",
+      });
 
   startTracing();
   const tracer = trace.getTracer(TRACER);
